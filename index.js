38872ea28258f5ea8a3f5349a75b5f55e8ab54f1
Dean Mabuela <dean.mabuela@gmail.com>
2022-11-01 17:30:30 +0200

    added initial files

---
+++ index.js
@@ -0,0 +1,29 @@
+// const time = "evening";
+
+// switch (time) 
+// {
+//     case "morning":
+//     console.log("Good Morning")
+//     break;
+//     case "afternoon":
+//     console.log("Good Afternoon")
+//     break;
+//     case "evening":
+//     console.log("Good Evening")
+//     default:
+//     console.log("HI")
+// }
+
+// console.log("DONE");
+
+console.log()
+// Boolean(Nan)
+// false === false
+// 23 === 23
+// "hello" == "hello"
+// "Hello" === "Hello"
+// Boolean(0)
+// Boolean("0")
+// Boolean("")
+// !null
+// !!"hello"
